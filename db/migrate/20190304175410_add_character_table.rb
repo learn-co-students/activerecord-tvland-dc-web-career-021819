a9c9a68c7f9a3b4d05998ca7317d5b5f47c8f04d
Sean <shonkelli@gmail.com>
2019-03-04 13:33:10 -0500

    Done.

---
+++ db/migrate/20190304175410_add_character_table.rb
@@ -0,0 +1,11 @@
+class AddCharacterTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :characters do |t|
+      t.string :catchphrase
+      t.string :name
+      t.integer :actor_id
+      t.integer :show_id
+      t.timestamps
+    end
+  end
+end
